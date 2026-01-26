af1d6f44f77cf413692fbf68d1e538dc61fe2751
Wallace Vieira da Silva <49287183+wallyboy22@users.noreply.github.com>
2026-01-26 14:36:18 -0300

    Create A_0_0_oficial_gitclone.py for repository cloning
    Add script to clone MapBiomas Fire repository.

---
+++ collection_010/classification_algorithms/A_0_0_oficial_gitclone.py
@@ -0,0 +1,4 @@
+# last_update: '2026/01/26', github:'mapbiomas/chile-fire', source: 'IPAM', contact: 'contato@mapbiomas.org'
+# MapBiomas Fire Classification Algorithms Step A_0_0_oficial_gitclone.py 
+### Step A_0_0: Clone the official MapBiomas Fire Network GitHub repository to the temporary disk of Google Colab
+!git clone https://github.com/mapbiomas/chile-fire
