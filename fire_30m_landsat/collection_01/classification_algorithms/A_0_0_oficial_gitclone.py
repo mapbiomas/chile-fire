23c3705bd2f8681b4da4ca6c9a563e6e61ba682f
Wallace Vieira da Silva <49287183+wallyboy22@users.noreply.github.com>
2026-02-12 14:18:19 -0300

    Update A_0_0_oficial_gitclone.py

--- fire_30m_landsat/collection_01/classification_algorithms/A_0_0_oficial_gitclone.py
+++ fire_30m_landsat/collection_01/classification_algorithms/A_0_0_oficial_gitclone.py
@@ -2,3 +2,7 @@
 # MapBiomas Fire Classification Algorithms Step A_0_0_oficial_gitclone.py 
 ### Step A_0_0: Clone the official MapBiomas Fire Network GitHub repository to the temporary disk of Google Colab
 !git clone https://github.com/mapbiomas/chile-fire
+
+
+### Step A_0_0 - Clone the official MapBiomas Fire Network GitHub repository to the temporary disk of Google Colab
+# !git clone --branch fit-otimizations_how_tf2 https://github.com/mapbiomas/chile-fire
